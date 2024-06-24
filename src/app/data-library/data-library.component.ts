30bf13070401346a315f13baa3b18a614e54b429
Dhinesh-Siva-Kumar <dhineshsanty366@gmail.com>
2024-06-24 10:52:15 +0530

    kick start

---
+++ src/app/data-library/data-library.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-library',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-library.component.html',
+  styleUrl: './data-library.component.css'
+})
+export class DataLibraryComponent {
+
+}
